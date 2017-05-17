677da7ea0d79c00846293d42592d8b1e9e5fd842
Lawal Lanre <larrystonegroup@gmail.com>
2017-05-17 22:22:31 +0100

    implement login and signup UI on index.html (Javascript function)

---
+++ public/js/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let shownSignUpSection = false;
+
+let hideShow = () => {
+    if (shownSignUpSection) {
+        document.querySelector('#login').style.visibility = 'visible';
+        document.querySelector('#signup').style.visibility = 'hidden';
+        document.querySelector('#password2').style.display = 'none';
+
+        document.querySelector('#account_action').innerHTML = 'Create account';
+        
+        shownSignUpSection = !shownSignUpSection;
+    } else {
+        document.querySelector('#login').style.visibility = 'hidden';
+        document.querySelector('#signup').style.visibility = 'visible';
+        document.querySelector('#password2').style.display = 'block';
+        
+        document.querySelector('#email').value = '';
+        document.querySelector('#password1').value = '';
+
+        document.querySelector('#account_action').innerHTML = 'Login';
+
+        shownSignUpSection = !shownSignUpSection;
+    }
+};
